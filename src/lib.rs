f969f7d976082126597abf8ed2f67f736f8ce8ea
Volker Mische <volker.mische@gmail.com>
2020-09-08 15:17:47 +0200

    feat: export storage proofs errors
    This way the FFI has access to specific storage proofs errors.

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ mod types;
 pub use crate::registry::{RegisteredPoStProof, RegisteredSealProof, Version};
 pub use crate::types::{PrivateReplicaInfo, PublicReplicaInfo};
 
+pub use filecoin_proofs_v1::storage_proofs::error::Error as StorageProofsError;
 pub use filecoin_proofs_v1::storage_proofs::fr32;
 pub use filecoin_proofs_v1::storage_proofs::post::election::Candidate;
 pub use filecoin_proofs_v1::storage_proofs::sector::{OrderedSectorSet, SectorId};
