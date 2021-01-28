dddf93bda2f78f7f0ed2eb8cb10f7876e594bc44
nemo <nemo@protocol.ai>
2021-01-28 15:10:12 -0500

    style: rust fmt

--- src/lib.rs
+++ src/lib.rs
@@ -12,12 +12,12 @@ mod types;
 pub use crate::registry::{RegisteredPoStProof, RegisteredSealProof};
 pub use crate::types::{PrivateReplicaInfo, PublicReplicaInfo};
 
-pub use fr32;
-pub use storage_proofs_core::api_version::ApiVersion;
-pub use storage_proofs_core::error::Error as StorageProofsError;
-pub use storage_proofs_core::sector::{OrderedSectorSet, SectorId};
 pub use filecoin_proofs_v1::types::{
     ChallengeSeed, Commitment, PaddedBytesAmount, PieceInfo, PoStType, ProverId, Ticket,
     UnpaddedByteIndex, UnpaddedBytesAmount,
 };
 pub use filecoin_proofs_v1::{FallbackPoStSectorProof, SnarkProof, VanillaProof};
+pub use fr32;
+pub use storage_proofs_core::api_version::ApiVersion;
+pub use storage_proofs_core::error::Error as StorageProofsError;
+pub use storage_proofs_core::sector::{OrderedSectorSet, SectorId};
