716c64d8ca46101efa08f685897c128056274075
nemo <nemo@protocol.ai>
2021-03-09 16:59:21 -0500

    feat: update to latest proofs dependencies fix: update proofs code to accomodate for the v6.x re-factors

--- src/seal.rs
+++ src/seal.rs
@@ -3,6 +3,7 @@ use std::io::{Read, Seek, Write};
 use std::path::{Path, PathBuf};
 
 use anyhow::{bail, ensure, Error, Result};
+use bellperson::bls::Fr;
 use filecoin_hashers::Hasher;
 
 use filecoin_proofs_v1::constants::{
@@ -489,7 +490,7 @@ fn seal_commit_phase1_inner<Tree: 'static + MerkleTreeTrait>(
         ticket,
     } = output;
 
-    let replica_id: bellperson::bls::Fr = replica_id.into();
+    let replica_id: Fr = replica_id.into();
     Ok(SealCommitPhase1Output {
         registered_proof,
         vanilla_proofs: VanillaSealProof::from_raw::<Tree>(registered_proof, &vanilla_proofs)?,
@@ -536,7 +537,7 @@ fn seal_commit_phase2_inner<Tree: 'static + MerkleTreeTrait>(
     } = phase1_output;
 
     let config = registered_proof.as_v1_config();
-    let replica_id: bellperson::bls::Fr = replica_id.into();
+    let replica_id: Fr = replica_id.into();
 
     let co = filecoin_proofs_v1::types::SealCommitPhase1Output {
         vanilla_proofs: vanilla_proofs.try_into()?,
